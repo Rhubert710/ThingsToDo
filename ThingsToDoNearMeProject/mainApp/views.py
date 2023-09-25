fce5e7e7e2914f368f30e1eec4ddc38598fb895e
Robert Hubert <rhubert710@gmail.com>
2023-09-25 03:46:29 -0400

    added 'addEventForm.html' via button styled <a>tag

--- ThingsToDoNearMeProject/mainApp/views.py
+++ ThingsToDoNearMeProject/mainApp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def indexDemo(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
-    context={}
+    context={ 'pageTitle' : pageTitle} 
     return render(request, "mainApp/index.html", context)
+
+def addEventForm(request):
+    return render(request, "mainApp/addEventForm.html")
+
+pageTitle = 'Things-to-do-near-me!'
 
 
