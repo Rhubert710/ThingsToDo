e8131d0bee94e614bd71862535e72396f917bf49
Robert Hubert <rhubert710@gmail.com>
2023-09-29 01:01:55 -0400

    milestone: added SeatGeek API to getEvents view and adjusted index.html accordingly

--- ThingsToDoNearMeProject/mainApp/views.py
+++ ThingsToDoNearMeProject/mainApp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse, JsonResponse
 
 from mainApp.models import *
-import json
-import requests , datetime
+import json, requests , datetime
 
 
 def indexDemo(request):
@@ -15,9 +14,44 @@ def index(request):
     # print(request.headers)
     return render(request, "mainApp/index.html", context)
 
+
+
+
+
+
+
 def getEvents(request):
-    l = Event.objects.filter(date__gte = datetime.date.today()).values() #Time on db seems to be three hours ahead of local time.
-    # print(datetime.date.today())
+    
+    ## Get Events from OUR DB
+    l = list( Event.objects.filter(date__gte = datetime.date.today()).values() ) #Time on db seems to be three hours ahead of local time.
+    print(datetime.date.today()+datetime.timedelta(days=4))
+
+    
+    ## Get from seat geek api
+    results_page_number, seatGeekJson = 0, []
+    while(True):
+        results_page_number += 1
+        r = requests.get(f'https://api.seatgeek.com/2/events?geoip=true&datetime_local.gt={datetime.date.today()}&datetime_local.lte={datetime.date.today()+datetime.timedelta(days=4)}&per_page=50&page={results_page_number}&client_id=MzcwNDI1NTB8MTY5NTg3NDM1My4wNjYwMDU1')
+        data = r.json()
+
+        for o in data['events']:
+            temp_obj = {}
+            temp_obj['title'] = ''.join(o["title"].split('-')[:-1]) if len(o["title"].split('-')) > 2 else o["title"].split('-')[0]
+            temp_obj['date'] = o["datetime_local"][:10]
+            temp_obj['address'] = o["venue"]["name"]
+            temp_obj['latitude'] = o['venue']["location"]["lat"]
+            temp_obj['longitude'] = o['venue']['location']['lon']
+            seatGeekJson.append(temp_obj)
+
+        # seatGeekJson += list ( data['events'] )
+        if len(data['events']) == 0:
+            break
+    # print(seatGeekJson)
+
+
+    #Combine All Results
+    l += seatGeekJson
+    print(len(l))
     return JsonResponse(list(l), safe=False)
 
 def addEventForm(request):
