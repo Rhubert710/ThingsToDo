c4138cbc7406fa7fe17676b4fe57273afa86673a
Robert Hubert <rhubert710@gmail.com>
2023-10-09 23:16:12 -0400

    added address input in index.html

--- ThingsToDoNearMeProject/mainApp/views.py
+++ ThingsToDoNearMeProject/mainApp/views.py
@@ -21,17 +21,21 @@ def index(request):
 
 
 def getEvents(request):
+
+    ## Get clients ip for lat , lng:
+    client_ip = get_client_ip(request)
+    print(client_ip)
     
     ## Get Events from OUR DB
-    l = list( Event.objects.filter(date__gte = datetime.date.today()).values() ) #Time on db seems to be three hours ahead of local time.
-    # print(datetime.date.today()+datetime.timedelta(days=4))
+    l = list( Event.objects.filter(date__gte = datetime.date.today()).values() ) #Time on db seems to be three hours ahead of local time.(its UTC)
+    # print(datetime.date.today()+datetime.timedelta(days=6))
 
     
     ## Get from seat geek api
     results_page_number, seatGeekJson = 0, []
     while(True):
         results_page_number += 1
-        r = requests.get(f'https://api.seatgeek.com/2/events?geoip=68.175.41.153&datetime_local.gt={datetime.date.today()}&datetime_local.lte={datetime.date.today()+datetime.timedelta(days=4)}&per_page=50&page={results_page_number}&client_id=MzcwNDI1NTB8MTY5NTg3NDM1My4wNjYwMDU1')
+        r = requests.get(f'https://api.seatgeek.com/2/events?geoip=68.175.41.153&datetime_local.gt={datetime.date.today()}&datetime_local.lte={datetime.date.today()+datetime.timedelta(days=6)}&per_page=50&page={results_page_number}&client_id=MzcwNDI1NTB8MTY5NTg3NDM1My4wNjYwMDU1')
         data = r.json()
 
         for o in data['events']:
@@ -64,18 +68,26 @@ def postEvent(request):
     newEvent = Event(title=p['titleInput'], description=p['descriptionInput'], date=p['dateInput'], address=p['addressInput'])
 
     #get geocode
-    r = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={p["addressInput"]}&key=AIzaSyBsjbLLe2RaAjIzUe5lxKb7wLFvebnX2gY')
+    geocode = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={p["addressInput"]}&key=AIzaSyBsjbLLe2RaAjIzUe5lxKb7wLFvebnX2gY')
     # print(r.text)
-    rj= json.loads(r.text)
+    geocode_json= json.loads(geocode.text)
     # print(rj['results'][0]['geometry']['location'])
     # print(rj['results'][0]['geometry']['location']['lat'])
 
-    newEvent.latitude = rj['results'][0]['geometry']['location']['lat']
-    newEvent.longitude = rj['results'][0]['geometry']['location']['lng']
+    newEvent.latitude = geocode_json['results'][0]['geometry']['location']['lat']
+    newEvent.longitude = geocode_json['results'][0]['geometry']['location']['lng']
 
     newEvent.save()
     return render(request, "mainApp/index.html")
 
 pageTitle = 'Things-to-do-near-me!'
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+       ip = x_forwarded_for.split(',')[0]
+    else:
+       ip = request.META.get('REMOTE_ADDR')
+       print(11)
+    return ip
 
